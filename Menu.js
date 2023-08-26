dceff1b006ca025d5c915e85fe604d8c187ffb22
Moskwa Quentin moskwaquentin@gmail.com <95235691+QuentinMoskwa@users.noreply.github.com>
2023-08-26 15:51:40 +0200

    Ajout de changement de couleur et de transitions.

--- Menu.js
+++ Menu.js
@@ -15,7 +15,34 @@ $(document).ready(function()
                 menuIsIn = false;
             }
         });
-
+        $(".lien1").hover(
+          function () {
+            $(".menuContainer").addClass("firstLienColor");
+          },
+          function () {
+            // on mouseout, reset the background colour
+            $(".menuContainer").removeClass("firstLienColor");
+          }
+        );
+        $(".lien2").hover(
+          function () {
+            $(".menuContainer").addClass("secondLienColor");
+          },
+          function () {
+            // on mouseout, reset the background colour
+            $(".menuContainer").removeClass("secondLienColor");
+          }
+        );
+        $(".lien3").hover(
+          function () {
+            $(".menuContainer").addClass("thirdLienColor");
+          },
+          function () {
+            // on mouseout, reset the background colour
+            $(".menuContainer").removeClass("thirdLienColor");
+          }
+        );
+        
     })
 
 })
